f0754821e1473a76c01632b5eb6555cc0210469b
Guy Cherubin <ngchrbn@gmail.com>
2023-06-19 17:44:19 +0300

    Remove birthPlace

--- resources/views/born-same-day.blade.php
+++ resources/views/born-same-day.blade.php
@@ -28,9 +28,6 @@
                                         <p class="text-sm font-medium text-gray-900 truncate">
                                             {{$actor['name']}}
                                         </p>
-                                        <p class="text-sm text-gray-500 truncate">
-                                            {{$actor['birthPlace']}}
-                                        </p>
                                     </div>
                                     <div class="inline-flex items-center text-base font-semibold text-gray-900">
                                         {{$actor['birthDate']}}
